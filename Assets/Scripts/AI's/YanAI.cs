bc01fec6b9f1a370ad27249ca85edc98683cd3bf
MojtabaOHz <moji.ostad@gmail.com>
2021-03-11 12:11:27 +0100

    Add Yan and Ilja AI script

---
+++ Assets/Scripts/AI's/YanAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YanAI : BaseAI
+{
+    public override IEnumerator RunAI() {
+        for (int i = 0; i < 10; i++)
+        {
+            yield return Ahead(10);
+            yield return Fire(1);
+            yield return TurnTurretLeft(90);
+            yield return TurnLeft(360);
+            yield return Fire(1);
+            yield return TurnTurretRight(180);
+            yield return Back(10);
+            yield return Fire(1);
+            yield return TurnTurretLeft(90);
+            yield return TurnRight(90);
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override void OnScannedRobot(ScannedRobotEvent e)
+    {
+        Debug.Log("Ship detected: " + e.Name + " at distance: " + e.Distance);
+    }
+}
